d7a347efb95641161c6629b07a0806b8148da88f
daksh273 <pateldaksh712@gmail.com>
2021-03-21 18:05:26 +0530

    User CRUD in ionic app

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,23 @@ export class UserService {
   
   userName: any;
   isLoggedIn: any;
+  webAPI: string = environment.webAPI;
   private loginEvent = new Subject<any>();
 
   constructor(public http: HttpClient) { }
 
   getUsers(){
-      return this.http.get("https://reqres.in/api/users?page=2")
+      return this.http.get(this.webAPI+"?page=2")
         .pipe(map((response: any) => response))
   }
 
   deleteUser(userId){
-    return this.http.delete("https://reqres.in/api/users/"+userId)
+    return this.http.delete(this.webAPI+userId)
     .pipe(map((response: any) => response))
   }
 
   addUser(userData){
-    return this.http.post("https://reqres.in/api/users/", userData)
+    return this.http.post(this.webAPI, userData)
     .pipe(map((response: any) => response))
   }
 
